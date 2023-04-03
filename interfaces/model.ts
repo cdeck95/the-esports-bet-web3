34e6d173a6872710d5be79106c837a6ba7031c2a
Chris Deck <deckchris95@gmail.com>
2023-04-03 18:37:22 -0400

    initial starting point

---
+++ interfaces/model.ts
@@ -0,0 +1,63 @@
+
+
+export interface League {
+    id: string;
+    name: string;
+    cc: string;
+}
+
+export interface Home {
+    id: string;
+    name: string;
+    image_id: string;
+    cc?: any;
+}
+
+export interface Away {
+    id: string;
+    name: string;
+    image_id: string;
+    cc?: any;
+}
+
+export interface OAway {
+    id: string;
+    name: string;
+    image_id: string;
+    cc?: any;
+}
+
+export interface OHome {
+    id: string;
+    name: string;
+    image_id: string;
+    cc?: any;
+}
+
+export interface Events {
+    id: string;
+    sport_id: string;
+    time: string;
+    time_status: string;
+    league: League;
+    home: Home;
+    away: Away;
+    ss: string;
+    bet365_id: string;
+    o_away: OAway;
+    round: string;
+    o_home: OHome;
+}
+
+export interface Articles {
+    articles: Array<Article>;
+}
+
+export interface Article {
+    id: string;
+    label: string;
+    text: string;
+    imageSrc: string;
+    link: string;
+}
+    
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,58 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
+import { useMemo, useState } from "react";
 import styles from "../styles/Home.module.css";
+import { Events, Article, Articles, Away, Home, OAway, OHome, League} from '../interfaces/model';
 
-const Home: NextPage = () => {
+
+function HomePage (){
+
+
+//const APIKEY = process.env.API_KEY;
+const APIKEY = "119588-R5L48IwuRSWhmR";
+
+console.log(APIKEY);
+
+const [upcomingEvents, setUpcomingEvents] = useState<any>([]);
+const [liveEvents, setLiveEvents] = useState<any>([]);
+
+const getUpcomingMatches = useMemo(async () => {
+  try {
+    const url = `https://api.b365api.com/v3/events/upcoming?sport_id=151&token=${APIKEY}`
+    console.log(url);
+    const response = await fetch(url, {
+      method: 'GET'
+    });
+    console.log(response.status);
+    if(response.status != 200){
+      console.log(`error fetching upcoming games`)
+      return;
+    } 
+    const responseBody = JSON.stringify(response.body);
+    //setUpcomingEvents(responseBody)
+    console.log(responseBody);
+  } catch (e: any){
+    console.log(e.message)
+  }
+  
+}, []);
+
+// const getLiveMatches = useMemo(async () => {
+//   const response = await fetch(`https://api.b365api.com/v3/events/inplay?sport_id=151&token=${APIKEY}`, {
+//       method: 'GET'
+//   });
+//   console.log(response.status);
+//   if(response.status != 200){
+//     console.log(`error fetching live games`)
+//   } else {
+//     const responseBody = JSON.stringify(response.body);
+//     console.log(responseBody);
+//   }
+// }, []);
+
+
+
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
@@ -10,46 +60,26 @@ const Home: NextPage = () => {
           Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
         </h1>
 
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
-        </p>
+       
 
         <div className={styles.connect}>
           <ConnectWallet />
         </div>
 
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
+        {/* {upcomingEvents
+        ? upcomingEvents.map((event: any, index: number) => (
+            <div key={index}>
+              <h3>{event.name}</h3>
+              <p>{event.description}</p>
+            </div>
+          ))
+        : <div>Loading...</div>
+        } */}
 
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
+        
       </main>
     </div>
   );
 };
 
-export default Home;
+export default HomePage;
